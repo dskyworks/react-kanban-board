dec00b014e35eee20dc4ba328b7fb47b7b3b114a
Vladislav Dribinsky <74740307+dskyworks@users.noreply.github.com>
2022-12-26 15:08:44 +0300

    Added gh-pages

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>
